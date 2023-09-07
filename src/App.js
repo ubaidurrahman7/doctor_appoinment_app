19943e579f4129fb90c0dc5c715fd699371fea6e
Ubaid ur Rahman <ubaidurr888999@gmail.com>
2023-09-07 03:18:46 -0400

    conver html pages to React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LandingPage from './components/Landing_Page/LandingPage';
+import Navbar from './components/Navbar/Navbar';
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+          <Navbar />
+              <Routes>
+                <Route path='/' element={<LandingPage />} />
+              </Routes>
+            
+        </BrowserRouter>
+       
     </div>
   );
 }
