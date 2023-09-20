0815355d2236506271a91e17ba88daa52f69614c
Ubaid ur Rahman <ubaidurr888999@gmail.com>
2023-09-20 01:16:56 -0400

    added changes for deployment

--- src/config.js
+++ src/config.js
@@ -1,8 +1,1 @@
-export const API_URL = window.location.hostname === "localhost"
-  ? "http://localhost:3000" 
-  : "https://ubaidurr8889-8181.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
-
-console.log(
-    "API_URL :",
-    API_URL
-);
+export const API_URL = window.location.origin;
